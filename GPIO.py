acd4f7a1d69b893aa7678c239b8173a0d7df519d
eblackmon <eliseblackmon34@gmail.com>
2015-07-22 13:09:17 -0400

    Simulate Button Press
    Added a timer, set pin 23 to turn on and off

--- GPIO.py
+++ GPIO.py
@@ -5,23 +5,27 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 GPIO.setup(27, GPIO.IN)
 GPIO.setup(22, GPIO.IN)
-GPIO.setup(23, GPIO.IN)
+GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.IN)
 while True:
 	input_stateCapture1 = GPIO.input(17)
 	input_stateCapture2 = GPIO.input(27)
 	input_stateCapture3 = GPIO.input(22)
-	input_stateCapture4 = GPIO.input(23)
+	#input_stateCapture4 = GPIO.input(23)
 	input_stateCapture5 = GPIO.input(24)
 	time.sleep(1)
+	GPIO.output(23, 0)
+	time.sleep(5)
+	GPIO.output(23, 1)
+	time.sleep(5)
 	if input_stateCapture1 == True:
 		print('Pin1 Pressed')
 	if input_stateCapture2 == True:
 		print('Pin2 Pressed')
 	if input_stateCapture3 == True:
 		print('Pin3 Pressed')
-	if input_stateCapture4 == True:
-		print('Pin4 Pressed')
+	#if input_stateCapture4 == True:
+		#print('Pin4 Pressed')
 	if input_stateCapture5 == True:
 		print('Pin5 Pressed')
 	
